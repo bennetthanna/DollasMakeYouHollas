469d3114ba43ff1b39467cac025b12bed6a0bcf7
Hanna Bennett <bennetthanna22@gmail.com>
2019-01-12 21:45:20 -0800

    Allow CRUD operations on randomly generated notes

--- src/LoggedIn.js
+++ src/LoggedIn.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import firebase from './firebase.js';
+import uuid from 'uuid/v4';
+import { animals, adjectives, verbs, nouns, exclamations } from './randomWords';
 
 class LoggedIn extends Component {
   constructor(props) {
     super(props);
 
+    this.state = {
+      isLoaded: false,
+      notes: { },
+      currentNote: null
+    }
+
     const user = firebase.auth().currentUser;
 
     this.currentUser = {};
     this.logOut = this.logOut.bind(this);
+    this.createNote = this.createNote.bind(this);
+    this.updateNote = this.updateNote.bind(this);
+    this.deleteNote = this.deleteNote.bind(this);
+    this.selectNote = this.selectNote.bind(this);
 
     if (user != null) {
       this.currentUser.name = user.displayName;
@@ -19,9 +31,17 @@ class LoggedIn extends Component {
     }
   }
 
-  logOut(e) {
+  componentDidMount() {
+    firebase.database().ref('notes/').on('value', snapShot => {
+      const first = Object.keys(snapShot.val())[0];
+      const currentNote = this.state.currentNote || first;
+      this.setState({ isLoaded: true, notes: snapShot.val(), currentNote });
+    });
+  }
+
+  logOut(event) {
     const logOut = this.props.logOut;
-    e.preventDefault();
+    event.preventDefault();
     firebase.auth().signOut()
       .then(user => {
         logOut();
@@ -31,20 +51,88 @@ class LoggedIn extends Component {
       });
   }
 
+  createNote(event) {
+    const id = uuid();
+    firebase.database().ref(`notes/${id}`).set(this.generateNote());
+    this.setState({ currentNote: id });
+  }
+
+  generateNote() {
+    const title = `The ${this.generateRandomWord(adjectives)} ${this.generateRandomWord(animals)}`;
+    const body = `${title} ${this.generateRandomWord(verbs)} the ${this.generateRandomWord(nouns)} and exclaimed "${this.generateRandomWord(exclamations)}"!`;
+    return { title, body };
+  }
+
+  generateRandomWord(words) {
+    return words[Math.floor(Math.random() * Math.floor(words.length))];
+  }
+
+  updateNote(event, noteId) {
+    event.preventDefault();
+    firebase.database().ref(`notes/${noteId}`).update(this.generateNote());
+  }
+
+  deleteNote(event, noteId) {
+    event.preventDefault();
+    firebase.database().ref(`notes/${noteId}`).remove();
+    const notes = this.state.notes;
+    delete notes[noteId];
+    const first = Object.keys(notes)[0];
+    this.setState({ notes, currentNote: first });
+  }
+
+  selectNote(event, noteId) {
+    event.preventDefault();
+    this.setState({ currentNote: noteId });
+  }
+
+  renderList() {
+    const { notes } = this.state;
+    const selectNote = this.selectNote;
+    const currentNoteId = this.state.currentNote;
+    return (
+      <ul className="list-group list-group-flush">
+        {Object.keys(notes).map((noteId, index) => {
+          return (
+            <div key={index}>
+              <button type="button" className={currentNoteId === noteId ? 'list-group-item list-group-item-action active' : 'list-group-item list-group-item-action'} onClick={(event) => selectNote(event, noteId)}>{notes[noteId].title}</button>
+            </div>
+          )
+        })}
+      </ul>
+    );
+  };
+
   render() {
-    const user = this.currentUser;
-    const userDetails = Object.keys(this.currentUser).map(function(key) {
-      return <div><p>{key}: {user[key]}</p></div>
-    });
+    const currentNoteId = this.state.currentNote;
+    const currentNote = this.state.notes[currentNoteId];
 
     return (
-      <div className="container">
-      <div className="jumbotron">
-        <div className="text-center">
-          <p>Welcome {this.currentUser.email}!</p>
+      <div className="row">
+      <div className="card w-25">
+        <div className="card-header">
+          <button id="createNote" onClick={this.createNote}>Create Note</button>
         </div>
-        {userDetails}
-        <button id="logOut" onClick={this.logOut}>Log Out</button>
+        { this.state.isLoaded ? this.renderList() : null }
+      </div>
+      <div className="card w-75">
+        <div className="card-header">
+          <div className="row justify-content-around">
+            <div className="col">
+              <button id="updateNote" onClick={(event) => this.updateNote(event, currentNoteId)}>Update Note</button>
+            </div>
+            <div className="col">
+              <button id="deleteNote" onClick={(event) => this.deleteNote(event, currentNoteId)}>Delete Note</button>
+            </div>
+          </div>
+        </div>
+        <div className="card-body">
+          <h5 className="card-title">{ this.state.isLoaded ? `${currentNote.title}` : null }</h5>
+          <p className="card-text">{ this.state.isLoaded ? `${currentNote.body}` : null }</p>
+        </div>
+      </div>
+      <div className="card w-100 logOutButtonContainer">
+          <button className="logOutButton" id="logOut" onClick={this.logOut}>Log Out</button>
       </div>
     </div>
     );
---
+++ src/randomWords.js
@@ -0,0 +1,97 @@
+const animals = [
+  'elephant',
+  'loris',
+  'zebu',
+  'buffalo',
+  'jaguar',
+  'armadillo',
+  'chimpanzee',
+  'hedgehog',
+  'panda',
+  'sheep',
+  'narwhal',
+  'porpoise',
+  'starfish',
+  'lynx',
+  'alpaca'
+];
+
+const adjectives = [
+  'spooky',
+  'spiffy',
+  'special',
+  'alluring',
+  'peaceful',
+  'tremendous',
+  'tough',
+  'abashed',
+  'cynical',
+  'adaptable',
+  'flawless',
+  'vivacious',
+  'embarrassed',
+  'capable',
+  'exultant'
+];
+
+const verbs = [
+  'offended',
+  'named',
+  'kissed',
+  'grabbed',
+  'punished',
+  'replaced',
+  'separated',
+  'rolled',
+  'discovered',
+  'guided',
+  'offered',
+  'painted',
+  'supported',
+  'choked',
+  'deceived'
+];
+
+const nouns = [
+  'ocean',
+  'yarn',
+  'fairies',
+  'skirt',
+  'ring',
+  'salt',
+  'town',
+  'flower',
+  'maid',
+  'window',
+  'soda',
+  'poison',
+  'letter',
+  'spy',
+  'sidewalk'
+];
+
+const exclamations = [
+  'Oh boy',
+  'Holy guacamole',
+  'Whoopsie daisies',
+  'Yeah buddy',
+  'Yippee ki yay',
+  'Yeehaw',
+  'Holla',
+  'Boo yah',
+  'Zowie wowie',
+  'Va va voom',
+  'Oof',
+  'Phew',
+  'Golly gee whiz',
+  'Ooh la la',
+  'Dang nabbit'
+];
+
+module.exports = {
+  animals,
+  adjectives,
+  verbs,
+  nouns,
+  exclamations
+};
